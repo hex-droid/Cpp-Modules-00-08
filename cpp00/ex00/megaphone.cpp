fac5637e31f4a85c8c3840c2d2c1add6f90f4ddb
ztaouil <ztaouil@student.1337.ma>
2021-08-27 04:42:35 +0100

    new file:   cpp00/ex00/Makefile 	new file:   cpp00/ex00/megaphone.cpp

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+static char     *getBuffed(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] >= 97 && str[i] <= 122)
+            str[i] = str[i] - ('a' - 'A');
+        i++;
+    }
+    return (str);
+}
+
+int     main(int ac, char **av)
+{
+    int     i;
+
+    i = 1;
+    if (ac > 1)
+    { 
+        while (av[i])
+        {
+            std::cout << getBuffed(av[i]);
+            i++;
+        }
+        std::cout << "\n";    
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+}
